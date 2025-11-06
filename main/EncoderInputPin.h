eecc405173cd56e83b95f67a4de116a9f0650795
Liviu Gelea <liviu.gelea@gmail.com>
2025-11-06 18:09:56 +0200

    added forgotten file to repository

---
+++ main/EncoderInputPin.h
@@ -0,0 +1,46 @@
+#include "arduino.h"
+#include "Subscriber.h"
+#include "Pin.h"
+#include <vector>
+
+template<int gpio_number> class EncoderInputPin : public Pin {
+  public:
+
+    inline static bool state = false;
+
+    void attachTo(Subscriber *subscriber) override {
+        this->subscribers.push_back(subscriber);
+    }
+
+    EncoderInputPin() {
+        pinMode(gpio_number, INPUT);
+        attachInterrupt(gpio_number, subroutine, CHANGE);
+    }
+
+    static void subroutine() {
+        updateState();
+        notify(gpio_number);
+    }
+
+    int getPinNumber() override {
+        return gpio_number;
+    }
+
+    bool getState() override {
+        return state;
+    }
+
+    static void updateState() {
+        state = (bool)digitalRead(gpio_number);
+    }
+
+  protected:
+
+    inline static std::vector<Subscriber *> subscribers;
+
+    static void notify(int pin_number) {
+        for (int i = 0; i < subscribers.size(); i++) {
+            subscribers[i]->getUpdated(pin_number);
+        }
+    }
+};
